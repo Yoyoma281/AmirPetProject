33fc5eb12d62d403c7836f13e3a6beee8ac06749
Yoyoma281 <shai239@gmail.com>
2023-06-07 16:55:45 +0300

    AddedUnitTesting

---
+++ UnitTestingPetProject/FakeDataBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AmirPetProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AmirPetProject.Data;
+using Tests;
+
+namespace AmirPetProjectTests
+{
+    [TestClass]
+    public class FakeDataBase
+    {
+        public DB? DbContext { get; private set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<DB>()
+                 .UseInMemoryDatabase("F_DataBase")
+                 .Options;
+            
+            DbContext = new DB(options);
+
+        }
+        
+    }
+}
